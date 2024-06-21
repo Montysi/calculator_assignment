c90fbaa0a00b5ddad22d447d7bd85bed84bff16c
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-21 15:27:48 +0100

    added notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import { evaluate } from 'mathjs'
-import './App.css'
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
 
 const App = () => {
-
-  const [sum, setSum] = useState("")
+  const [sum, setSum] = useState("");
   // const buttons = ["1", "2", "3", "4","5", "6", "7", "8","9", "0","-", "+","*", "/", "C", "="]
   const buttons = ["C", "B", "()", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", ".", "0", "%", "="];
 
@@ -17,28 +16,33 @@ const App = () => {
       }
     } else if (btn === "C") {
       setSum("");
+    } else if (btn === "B") {
+      setSum(sum.slice(0, -1)); // makes the specified key act as a  backspace. -1 refers to the final character of a string (e.g -2 being second to last)
     } else {
-      setSum(sum + btn); // can't input at all without this else statement (figure out why)
+      setSum(sum + btn); // can't input at all without this else statement (is this because I have "C" and "B" set as specific conditions?)
     }
   };
 
   return (
-      <div className="centre">
+    <div className="centre">
       <div className="calcBody">
         <h4 className="inputBox">{sum}</h4>
         <div className="buttonWrap">
-          {
-            buttons.map((button, index) => {
-              return (
-                <button  key={index} onClick={() => handleClick(button)} className="btn">{button}</button>
-              )
-            })
-          }
+          {buttons.map((button, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => handleClick(button)}
+                className="btn"
+              >
+                {button}
+              </button>
+            );
+          })}
         </div>
       </div>
+    </div>
+  );
+};
 
-      </div>
-  )
-}
-
-export default App
+export default App;
