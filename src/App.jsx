cd632e3aa97eb734c4d4925d179d409fbbb25eac
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-21 14:41:45 +0100

    added if statement using 'evaluate' from mathjs

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ const App = () => {
 
   const [sum, setSum] = useState("")
   // const buttons = ["1", "2", "3", "4","5", "6", "7", "8","9", "0","-", "+","*", "/", "C", "="]
-  const buttons = ["C", "B", "()", "+", "7", "8", "9", "-", "4", "5", "6", "X", "1", "2", "3", "/", ".", "0", "%", "="];
+  const buttons = ["C", "B", "()", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", ".", "0", "%", "="];
 
- 
+  const handleClick = (btn) => {
+    if (btn === "=") {
+      try {
+        setSum(evaluate(sum).toString()); // evaluate creates a value based on sum (calc input), converts to a string then updates the state
+      } catch (error) {
+        setSum("Error");
+      }
+    } else if (btn === "C") {
+      setSum("");
+    } else {
+      setSum(sum + btn); // can't input at all without this else statement (figure out why)
+    }
+  };
 
   return (
       <div className="centre">
