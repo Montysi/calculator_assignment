c25348c54734e89b37fad0a65c8e96305b74b5b9
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-23 16:26:03 +0100

    added ANS button and some CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import "./App.css";
 
 const App = () => {
   const [sum, setSum] = useState("");
-  // const buttons = ["1", "2", "3", "4","5", "6", "7", "8","9", "0","-", "+","*", "/", "C", "="]
-  const buttons = ["C", "B", "()", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", ".", "0", "%", "="];
+  const [previousAns, setPreviousAns] = useState("");
+  const buttons = ["C", "B", "ANS", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", ".", "0", "%", "="];
 
   const handleClick = (btn) => {
     if (btn === "=") {
       try {
-        setSum(evaluate(sum).toString()); // evaluate creates a value based on sum (calc input), converts to a string then updates the state
+        let result = evaluate(sum).toString(); // evaluate creates a value based on sum (calc input), converts to a string then updates the state
+        setPreviousAns(result);
+        setSum(result);
       } catch (error) {
         setSum("Error");
       }
@@ -18,6 +20,8 @@ const App = () => {
       setSum("");
     } else if (btn === "B") {
       setSum(sum.slice(0, -1)); // makes the specified key act as a  backspace. -1 refers to the final character of a string (e.g -2 being second to last)
+    } else if (btn === "ANS") {
+      setSum(previousAns); 
     } else {
       setSum(sum + btn); // can't input at all without this else statement (is this because I have "C" and "B" set as specific conditions?)
     }
